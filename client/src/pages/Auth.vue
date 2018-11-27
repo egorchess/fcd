4e70ade9b0cb0c01a9e14a99724468120d3c61d6
egorchess <egor.tsakov@mail.ru>
2018-11-27 23:47:26 +0300

    Добавлен компонент "main_button" на странице авторизации

--- client/src/pages/Auth.vue
+++ client/src/pages/Auth.vue
@@ -1,11 +1,23 @@
 <template>
-  <div>Страница авторизации</div>
+  <div><h1>Страница авторизации</h1>
+    <v-button :onClick="testClick">Войти</v-button>
+  </div>
 </template>
 
 <script>
-export default {
-  title: 'Авторизация'
-};
+  import Button from './MainButton.vue';
+
+  export default {
+    title: 'Авторизация',
+    components: {
+      'v-button': Button
+    },
+    methods: {
+      testClick() {
+        // код обработчика
+      }
+    }
+  };
 </script>
 
 <style>
---
+++ client/src/pages/MainButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <button class="main_button" @click="onClick" >
+    <slot>Button</slot>
+  </button>
+</template>
+
+<script>
+  export default {
+    name: 'Button',
+    props: {
+      onClick: {
+        type: Function,
+        required: true
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  .main_button {
+    height: 50px;
+    width: 350px;
+    display: inline-block;
+    padding: 5px 25px 5px;
+    font-size: 30px;
+    cursor: pointer;
+    text-align: center;
+    text-decoration: none;
+    outline: none;
+    color: #fff;
+    background-color: #1FA19C;
+    border: 3px solid #509c99;
+    border-radius: 20px;
+    background: linear-gradient(to top, #087d78 20%, #1FA19C 50%, #64BAB7 80%);
+    box-shadow: 4px 4px 8px #64BAB7;
+  }
+  .main_button:hover {
+    background-color: #64BAB7;
+    background: linear-gradient(to top, #085f5a 20%, #1c837e 50%, #509c99 80%);
+  }
+</style>
